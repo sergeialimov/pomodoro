9eec6f2a3155b60fba8c47038811095cd4c37624
Sergei Alimov <8912652@gmail.com>
2019-02-19 20:01:11 +0300

    fix linter issues in render

--- src/App.jsx
+++ src/App.jsx
@@ -79,14 +79,14 @@ export default class App extends Component {
       if (!breakPaused) {
         breakTime.setSeconds(breakTime.getSeconds() - 1);
         this.setState({
-          break: breakTime,
+          breakTime,
         });
         if (breakTime.getMinutes() === 0 && breakTime.getSeconds() === 0) {
           setTimeout(() => {
             const { intervalId, breakCounter } = this.state;
             clearInterval(intervalId);
             this.setState({
-              break: new Date(new Date()
+              breakTime: new Date(new Date()
                 .setHours(0, breakCounter, 0, 0)),
               breakPaused: true,
               mode: 'session',
@@ -133,7 +133,7 @@ export default class App extends Component {
       session: new Date(new Date()
         .setHours(0, 25, 0, 0)),
       sessionCounter: 25,
-      break: new Date(new Date()
+      breakTime: new Date(new Date()
         .setHours(0, 5, 0, 0)),
       breakCounter: 5,
     });
@@ -172,7 +172,7 @@ export default class App extends Component {
     const hours = breakTime.getHours();
     if (sessionPaused && minutes < 60 && hours === 0) {
       this.setState({
-        break: new Date(new Date()
+        breakTime: new Date(new Date()
           .setHours(0, breakCounter + 1, 0, 0)),
         breakCounter: breakCounter + 1,
       });
@@ -192,16 +192,23 @@ export default class App extends Component {
   }
 
   render () {
-    const time = this.state.mode === 'session' ? this.state.session : this.state.breakTime;
-    const timerLabel = this.state.mode === 'session' ? 'Session' : 'Break';
+    const {
+      mode,
+      session,
+      breakTime,
+      breakCounter,
+      sessionCounter,
+    } = this.state;
+    const time = mode === 'session' ? session : breakTime;
+    const timerLabel = mode === 'session' ? 'Session' : 'Break';
     let seconds = time.getSeconds();
     if (seconds < 10) {
-      seconds = "0" + seconds
-    };
+      seconds = `0${seconds}`;
+    }
     let minutes = time.getMinutes();
     if (minutes < 10) {
-      minutes = "0" + minutes
-    };
+      minutes = `0${minutes}`;
+    }
 
     return (
       <div className="App">
@@ -216,16 +223,16 @@ export default class App extends Component {
                   id="break-decrement"
                   type="image"
                   src={arrowDown}
-                  alt={'break down'}
+                  alt="break down"
                   onClick={this.decreaseBreak}
                 />
-                <div id="break-length">{this.state.breakCounter}</div>
+                <div id="break-length">{breakCounter}</div>
                 <input
                   className="arrows"
                   id="break-increment"
                   type="image"
                   src={arrowUp}
-                  alt={'break up'}
+                  alt="break up"
                   onClick={this.increaseBreak}
                 />
               </div>
@@ -238,16 +245,16 @@ export default class App extends Component {
                   id="session-decrement"
                   type="image"
                   src={arrowDown}
-                  alt={'session down'}
+                  alt="session down"
                   onClick={this.decreaseSession}
                 />
-                <div id="session-length">{this.state.sessionCounter}</div>
+                <div id="session-length">{sessionCounter}</div>
                 <input
                   className="arrows"
                   id="session-increment"
                   type="image"
                   src={arrowUp}
-                  alt={'session up'}
+                  alt="session up"
                   onClick={this.increaseSession}
                 />
               </div>
@@ -255,9 +262,12 @@ export default class App extends Component {
           </div>
           <div id="timer">
             <div id="timer-label">{timerLabel}</div>
-            <p id="time-left">{minutes}:{seconds}</p>
+            <p id="time-left">{`${minutes}:${seconds}`}</p>
           </div>
-          <audio ref={this.beep} id='beep' src={soundPath}/>
+          <audio ref={this.beep} id="beep">
+            <source src={soundPath} />
+            <track label="English" kind="captions" src="https://media.martymcgui.re/.../subtitles.vtt" />
+          </audio>
           <div id="buttons">
             <div id="start_stop" onClick={this.startPause}>start/pause</div>
             <div id="reset" onClick={this.refresh}>refresh</div>
